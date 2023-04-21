bf057d2e3066df536c10bd48ae9a5e48e75f17c0
Shubodhraj <asubodh39@gmail.com>
2023-04-21 21:46:46 +0545

    first prototype

---
+++ construction-20230421T120800Z-001/construction/script.js
@@ -0,0 +1,70 @@
+$(document).ready(function(){
+    $(".navbar-toggler").click(function(){
+        $(".navbar-toggler").toggleClass("change");
+    })
+})
+
+
+
+function payment() {
+    var x = document.getElementById("click");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  function payment1() {
+    var x = document.getElementById("click1");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  function payment2() {
+    var x = document.getElementById("click2");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  
+  function payment4() {
+    var x = document.getElementById("clickk");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+ 
+
+  function pay(){
+    window.location = "payment.html";
+  }
+
+  
+  function payy(){
+    window.location = "pay.html";
+  }
+
+  function price(){
+    window.location = "video.html"
+  }
+
+  function price1(){
+    window.location = "price.html"
+  }
+
+  function log(){
+    window.location = "login1.html"
+  }
+
+  
+
+ 
